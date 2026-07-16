e39d3a9134cca827af2214423df7caa9f234a4d0
Divanshu Singh <divanshusingh726@gmail.com>
2026-07-16 19:15:53 +0530

    Printing datatpyes and format specifier

--- push.bat
+++ push.bat
@@ -4,17 +4,16 @@ echo ===============================
 echo Uploading to GitHub...
 echo ===============================
 
+git status
+
 git add .
 
-set /p msg=Enter Commit Message: 
+set /p msg=Enter Commit Message:
 
 git commit -m "%msg%"
 
 git push origin main
 
 echo.
-echo ===============================
-echo Upload Complete!
-echo ===============================
-
+echo Press any key to exit...
 pause
